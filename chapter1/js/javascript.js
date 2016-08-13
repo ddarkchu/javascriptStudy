55d89e66a2e30bb5862d29fe256977555759936f
portars <portars@naver.com>
2016-08-13 11:04:38 +0900

    update

--- chapter1/js/javascript.js
+++ chapter1/js/javascript.js
@@ -1,6 +1,14 @@
+// hljs.initHighlightingOnLoad();
+// hljs.initLineNumbersOnLoad();
 $(document).ready(function() {
 	$('#fullpage').fullpage();
+
 	$('pre code').each(function(i, block) {
 		hljs.highlightBlock(block);
+		hljs.lineNumbersBlock(block);
 	});
+	setInterval(function(){location.reload();},10000);
+	// $('code').each(function(i, block) {
+	//
+  //   });
 });
